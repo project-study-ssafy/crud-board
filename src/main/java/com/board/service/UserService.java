ab507b44f656a3e28e7647fb58d5e98efafb167c
jongchan0109 <jongchan1009@naver.com>
2024-09-28 16:28:36 +0900

    feat: 회원 가입시 로그인명, 닉네임 중복 확인 수정 #5

--- src/main/java/com/board/service/UserService.java
+++ src/main/java/com/board/service/UserService.java
@@ -3,6 +3,8 @@ package com.board.service;
 import com.board.domain.User;
 import com.board.dto.user.SignUpDto;
 import com.board.global.exception.user.DuplicatedLoginIdException;
+import com.board.global.exception.user.DuplicatedNicknameException;
+import com.board.global.exception.user.UserNotFoundException;
 import com.board.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -21,11 +23,14 @@ public class UserService {
         String password = user.getPassword();
         String nickname =  user.getNickname();
 
-        boolean exists = userRepository.existsByLoginId(loginId);
-
-        if (exists) {
+        if (userRepository.existsByLoginId(loginId)) {
             throw new DuplicatedLoginIdException("중복된 사용자입니다.");
         }
+
+        if (userRepository.existsByNickname(nickname)) {
+            throw new DuplicatedNicknameException("중복된 닉네임입니다.");
+        }
+
         User signUpuser = new User(loginId, password, nickname);
         return userRepository.save(signUpuser);
     }
