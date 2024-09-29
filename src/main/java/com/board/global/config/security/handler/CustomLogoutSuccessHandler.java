fd72281206828ccc3cd1fbb3fda5e3c144369a31
jongchan0109 <jongchan1009@naver.com>
2024-09-29 21:17:05 +0900

    feat: 로그 아웃 기능 구현 #15

---
+++ src/main/java/com/board/global/config/security/handler/CustomLogoutSuccessHandler.java
@@ -0,0 +1,22 @@
+package com.board.global.config.security.handler;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class CustomLogoutSuccessHandler implements LogoutSuccessHandler {
+
+    @Override
+    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response,
+        Authentication authentication) throws IOException {
+
+        log.info("[로그아웃]");
+        response.sendRedirect(request.getContextPath() + "/");
+    }
+}
