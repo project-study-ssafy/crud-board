93b2a448fb4ae8c0f307fbb51e2bc55c5cda57ac
UnrequiredOne <>
2024-10-09 22:28:58 +0900

    feat: 게시판 생성 시간 오름차순 정렬 구현

--- src/main/java/com/board/repository/BoardRepository.java
+++ src/main/java/com/board/repository/BoardRepository.java
@@ -2,9 +2,6 @@ package com.board.repository;
 
 import com.board.domain.Board;
 import java.util.List;
-import java.util.Optional;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
